9db9adaa103a1af9c3fc0c8eb05d3f84cdd6945d
Anugerah20 <nabilpangestu123@gmail.com>
2022-05-18 19:16:18 +0700

    membuat tampilan checkout

--- checkout.php
+++ checkout.php
@@ -1,1 +1,50 @@
-<h1>Checkout</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Cart Janji Kopi</title>
+   <!-- Icon Kopi Suka -->
+   <link rel="shortcut icon" href="assets/logo/favicon-16x16.png" type="logo/png">
+
+   <!-- Font Rubik -->
+   <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+   <!-- Link Fontawesome -->
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+   <!-- Bootstrap 5 CSS -->
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+
+   <!-- CSS Custom -->
+   <link rel="stylesheet" href="assets/css/custom.css" />
+
+   <!-- CSS Table Cart -->
+   <style>
+      th,tr,td {
+         border: 0;
+      }
+   </style>
+</head>
+
+<body>
+   <div class="container">
+
+      <!-- START: Judul Halaman Admin -->
+      <div class="container-fuild">
+         <div style="margin-top: 3.5rem;">
+            <h2>Checkout</h2>
+      </div>
+   </div>
+   <hr>
+
+      
+</div>
+        
+   <!-- Popper dan Bootstrap 5 JS -->
+   <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
