0f43d775772a47a3ea625b8513962c3d0e2ed2ea
Nabil Anugerah <nabilpangestu123@gmail.com>
2022-09-08 08:40:31 +0700

    memperbarui font icon

--- Layouts/navigasi.php
+++ Layouts/navigasi.php
@@ -24,7 +24,7 @@ error_reporting(0);
   <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
   <!-- Link Fontawesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <!-- Bootstrap 5 CSS -->
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
